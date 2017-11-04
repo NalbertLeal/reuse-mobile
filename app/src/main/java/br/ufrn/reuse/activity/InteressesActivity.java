9f097ee8a7c47f1a0f76b35e2da8d2a955867393
Adelino Afonso <aafavelino@icloud.com>
2017-11-04 13:54:43 -0300

    Navigation Drawer

--- app/src/main/java/br/ufrn/reuse/activity/InteressesActivity.java
+++ app/src/main/java/br/ufrn/reuse/activity/InteressesActivity.java
@@ -1,14 +1,34 @@
 package br.ufrn.reuse.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import br.ufrn.reuse.R;
 
 public class InteressesActivity extends AbstractActivity {
-
+    private String[] situacao = new String[] {"Em análise"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interesses);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, situacao);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.situacao);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicao, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
