1b3f6aa601e35ef860844b5b33d8d3f724fa55d7
Adelino Afonso <aafavelino@icloud.com>
2017-11-05 14:44:28 -0300

    Interesses...

--- app/src/main/java/br/ufrn/reuse/activity/InteressesActivity.java
+++ app/src/main/java/br/ufrn/reuse/activity/InteressesActivity.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import java.util.List;
+
 import br.ufrn.reuse.R;
+import br.ufrn.reuse.activity.vitrine.VitrineListAdapter;
+import br.ufrn.reuse.dominio.anuncio.Anuncio;
+import br.ufrn.reuse.facade.ReuseFacadeImpl;
 
 public class InteressesActivity extends AbstractActivity {
     private String[] situacao = new String[] {"Todos", "Em análise", "Aprovados", "Aguardando aprovação"};
@@ -30,5 +37,17 @@ public class InteressesActivity extends AbstractActivity {
 
             }
         });
+
+
+        List<Anuncio> anuncios = new ReuseFacadeImpl(this).findAllAnunciosPublicados();
+        GridView listView = (GridView) findViewById(R.id.lista_interesses);
+        listView.setAdapter(new VitrineListAdapter(this, anuncios));
+
+        LinearLayout interessesLayout = (LinearLayout) findViewById(R.id.layout_interesses);
+
+        int widthIntereesses = interessesLayout.getWidth();
+
+
+        listView.setMinimumWidth(widthIntereesses);
     }
 }
