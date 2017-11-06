4438a0d4e38553f94c20a7fecccac6d99d2d1901
Adelino Afonso <aafavelino@icloud.com>
2017-11-06 15:48:52 -0300

    Interesses...

--- app/src/main/java/br/ufrn/reuse/activity/Vitrine.java
+++ app/src/main/java/br/ufrn/reuse/activity/Vitrine.java
@@ -30,9 +30,8 @@ public class Vitrine extends AbstractActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
